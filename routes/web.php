14d2b36cbe395669f95d88b7597cf3903527f882
teguhpermadi <tguh.permadi@gmail.com>
2025-11-15 08:56:42 +0700

    install package livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Livewire\UserManagement;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
@@ -30,6 +29,4 @@ Route::middleware(['auth'])->group(function () {
             ),
         )
         ->name('two-factor.show');
-
-        Route::get('users', UserManagement::class)->name('users.index');
 });

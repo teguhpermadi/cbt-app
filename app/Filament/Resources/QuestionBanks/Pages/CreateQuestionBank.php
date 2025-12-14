fc7f6b0cc9c0999976a1ec3ac36a9f022bc8c9e6
teguhpermadi <tguh.permadi@gmail.com>
2025-12-14 12:09:40 +0700

    feat: Add Filament form schema and create page for question banks.

--- app/Filament/Resources/QuestionBanks/Pages/CreateQuestionBank.php
+++ app/Filament/Resources/QuestionBanks/Pages/CreateQuestionBank.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateQuestionBank extends CreateRecord
 {
     protected static string $resource = QuestionBankResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('page-question-banks', ['record' => $this->record->id]);
+    }
 }
--- app/Filament/Resources/QuestionBanks/Schemas/QuestionBankForm.php
+++ app/Filament/Resources/QuestionBanks/Schemas/QuestionBankForm.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\QuestionBanks\Schemas;
 
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
 
 class QuestionBankForm
@@ -12,12 +15,21 @@ class QuestionBankForm
     {
         return $schema
             ->components([
+                Hidden::make('user_id')
+                    ->default(auth()->id()),
                 TextInput::make('name')
                     ->required()
                     ->maxLength(255),
                 Textarea::make('description')
                     ->required()
                     ->rows(3),
+                Select::make('subject_id')
+                    ->relationship('subject', 'name')
+                    ->required(),
+                Toggle::make('is_public')
+                    ->required()
+                    ->inline(false)
+                    ->hint('Jika diaktifkan, bank soal dapat dikoreksi oleh guru lain.'),
             ]);
     }
 }
